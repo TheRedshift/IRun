f977fa491d12b17be4375a60ac2eb6004238e2b3
psyrs11@nottingham.ac.uk <psyrs11@nottingham.ac.uk>
2018-01-06 17:43:29 +0000

    Added code to allow stats screen to move to other screens (TODO add navbar to stats + info screen)

--- app/src/main/java/com/example/rahulsoni/irun/StatsActivity.java
+++ app/src/main/java/com/example/rahulsoni/irun/StatsActivity.java
@@ -1,13 +1,52 @@
 package com.example.rahulsoni.irun;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
-public class StatsActivity extends AppCompatActivity {
+import static com.example.rahulsoni.irun.MainActivity.STATS;
+import static com.example.rahulsoni.irun.MainActivity.INFO;
+
+
+public class StatsActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+
+        //The following code populates the fields if they existed in the bundle, otherwise leaves them blank
+        Bundle bundle = getIntent().getExtras();
+
+
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_run) {
+            Log.d("g53mdp", "Run button pressed");
+            Intent intent = new Intent(StatsActivity.this, MainActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_stats) {
+            Log.d("g53mdp", "Stats button pressed");
+        } else if (id == R.id.nav_info) {
+            Log.d("g53mdp", "Info button pressed");
+            Intent intent = new Intent(StatsActivity.this, StatsActivity.class);
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
